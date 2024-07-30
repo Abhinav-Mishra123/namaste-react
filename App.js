a28b5ad34ec8166010275bc02e61f12d7126b84f
Abhinav-Mishra123 <contactatabhinavmishra@gmail.com>
2024-07-30 20:20:41 +0530

    episode-02 and 03 code

--- App.js
+++ App.js
@@ -1,36 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-{/* <div className="container">
-<div className="row">
-    <div className="first-heading">
-        <h1>This is first heading</h1>
-        <p>This is first of my paragraph</p>
+
+const Heading = () => (
+    <div className="container">
+      <div className="row">
+        <div className="first-heading">
+          <h1>This is the first heading</h1>
+          <p>This is the first paragraph</p>
+        </div>
+        <div className="second-heading">
+          <h1>This is the second heading</h1>
+          <p>This is the second paragraph</p>
+        </div>
+      </div>
     </div>
-    <div className="second-heading">
-        <h1>This is second heading</h1>
-        <p>This is second of my paragraph</p>
-    </div>
-</div>
-</div> */}
+  );
+
+  const number = 1000
+
+  const HeadingComponent = () =>(
+    <>
+   <h1>{number}</h1> 
+    <h1>This is Heading Component</h1>
+    <Heading props="1" /> 
+    <Heading></Heading>
+    {Heading()}
+    </>
+  )
+
+// const heading = React.createElement("div", {className: "container"},
+//     React.createElement("div", {className: "row"},
+//         [
+//             React.createElement("div", {className: "first-heading"},
+
+//                 [
+//                     React.createElement("h1", {},"This is first heading"),
+//                     React.createElement("p", {}, "This is first of my paragraph")
+//                 ],
+//             ),
+//             React.createElement("div", {className: "second-heading"},
+//                 [
+//                     React.createElement("h1", {}, "This is second headinasg sas"),
+//                     React.createElement("p", {}, "This is second of my paragraph asa")
+//                 ]
+//             ),
+//         ]
+//     ));
 
 
-const heading = React.createElement("div", {className: "container"},
-    React.createElement("div", {className: "row"},
-        [
-            React.createElement("div", {className: "first-heading"},
 
-                [
-                    React.createElement("h1", {},"This is first heading"),
-                    React.createElement("p", {}, "This is first of my paragraph")
-                ],
-            ),
-            React.createElement("div", {className: "second-heading"},
-                [
-                    React.createElement("h1", {}, "This is second heading"),
-                    React.createElement("p", {}, "This is second of my paragraph")
-                ]
-            ),
-        ]
-    ));
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(<HeadingComponent/>)
